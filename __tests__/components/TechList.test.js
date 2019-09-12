da9d841d2e7443a60801c31f3ea071c86d4f155b
Gabriel Lima <gabriellfsouza@gmail.com>
2019-09-12 06:18:57 -0300

    chore: mobile tests - green

--- __tests__/components/TechList.test.js
+++ __tests__/components/TechList.test.js
@@ -6,11 +6,12 @@ import {render, fireEvent} from '@testing-library/react-native';
 
 describe('TechList', () => {
   it('should be able to add new tech', () => {
-    const {getByText, getByTestId} = render(<TechList />);
+    const {getByTestId, debug, getByText} = render(<TechList />);
 
     fireEvent.changeText(getByTestId('tech-input'), 'Node.js');
     fireEvent.press(getByText('Adicionar'));
 
     expect(getByText('Node.js')).toBeTruthy();
+    expect(getByTestId('tech-input')).toHaveProp('value', '');
   });
 });
